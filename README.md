18e9a47ef8a4d6aca370948e99d39592a9638e79
Kshitij A <kshitij.everest@gmail.com>
2022-03-14 00:44:52 +0530

    Filters Updated

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# fun-with-filters
-Using OpenCV and np.arrays to understand the effect of filters on photos.
+# Basic Movie Recommender
+### Using OpenCV and np.arrays to understand the effect of filters on photos. 
+
+* [Use this app here](https://share.streamlit.io/horizon3902/fun-with-filters/main/app.py)
+
+#### OR
+
+* To run the app: - 
+
+  1. Clone the repo
+
+  2. Run `pip install -r requirements.txt` in shell
+
+  3. Run `streamlit run app.py` in shell
+
+
+### Kshitij Agarkar, IITN, 2021
+
+### Contact: -	
+
+<p align='center'>
+  <a href="mailto:bt20cse209@iiitn.ac.in">
+    <img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" />
+  </a>&nbsp;&nbsp;
+  <a href="https://instagram.com/kshitij_agarkar">
+    <img src="https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white" />        
+  </a>&nbsp;&nbsp;
+  <a href="https://github.com/horizon3902">
+    <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />
+</p>
--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+# from black import out
 import streamlit as st
 import numpy as np
 import cv2
 from PIL import Image, ImageEnhance
 import matplotlib.pyplot as plt
+
+def gkern(l=5, sig=1.):
+    """
+    creates gaussian kernel with side length `l` and a sigma of `sig`
+    """
+    ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
+    gauss = np.exp(-0.5 * np.square(ax) / np.square(sig))
+    kernel = np.outer(gauss, gauss)
+    return kernel / np.sum(kernel)
 
 st.set_page_config(page_title="Fun With Filters", page_icon="📸")
 st.title("Fun with Filters")
@@ -24,13 +34,14 @@ if upl_img is not None:
     
     with col2:
         st.write("Edited")
-        filter = st.sidebar.radio('What filter to use?: ', ['Original','Greyscale', 'Blur', 'Motion Blur','Vignette','Pencil Sketch','Emboss'])
-        
+        # filter = st.sidebar.radio('What filter to use?: ', ['Original','Greyscale', 'Blur', 'Motion Blur','Vignette','Pencil Sketch','Emboss'])
+        filter = st.sidebar.radio('What filter to use?: ', ['Original','Greyscale', 'Blur', 'Motion Blur','Pencil Sketch','Sharpen'])
+        conv_img = np.array(image.convert('RGB'))
+
         if filter == 'Original':
             st.image(image, width=300)
         
         elif filter=='Greyscale':
-            conv_img = np.array(image.convert('RGB'))
             r,g,b = conv_img[:,:,0], conv_img[:,:,1], conv_img[:,:,2]
             gamma = st.sidebar.slider("Gamma",0.0, 3.0, 1.04)
             rc, gc, bc = 0.2989, 0.5870, 0.1140
@@ -44,22 +55,12 @@ if upl_img is not None:
             st.image(show_img, clamp=True, width=300)
 
         elif filter=='Blur':
-            conv_img = np.array(image.convert('RGB'))
-            def gkern(l=5, sig=1.):
-                """
-                creates gaussian kernel with side length `l` and a sigma of `sig`
-                """
-                ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
-                gauss = np.exp(-0.5 * np.square(ax) / np.square(sig))
-                kernel = np.outer(gauss, gauss)
-                return kernel / np.sum(kernel)
             sig = st.sidebar.slider("Intensity: ",0.1,2.0,1.)
             kernel = gkern(10, sig)
             blur_image = cv2.filter2D(conv_img, -1, kernel)
             st.image(blur_image, channels='RGB', width=300)
         
         elif filter=='Motion Blur':
-            conv_img = np.array(image.convert('RGB'))
             i = st.sidebar.slider("Intensity: ",1,20,5)
             kernel = np.zeros((i,i))
             np.fill_diagonal(kernel, 1)
@@ -67,9 +68,82 @@ if upl_img is not None:
             mblur_image = cv2.filter2D(conv_img, -1, kernel)
             st.image(mblur_image, channels='RGB', width=300)
         
-        elif filter=='Vignette':
-            conv_img = np.array(image.convert('RGB'))
-    
+        # elif filter=='Vignette':
+        #     image.save('vig_img.jpg')
+        #     conv_img = cv2.imread('vig_img.jpg')
+        #     rows, cols = conv_img.shape[:2]
+
+        #     # generating vignette mask using Gaussian kernels
+        #     kernel_x = cv2.getGaussianKernel(cols,200)
+        #     kernel_y = cv2.getGaussianKernel(rows,200)
+        #     #rowsXcols
+        #     kernel = kernel_y * kernel_x.T
+
+        #     #Normalizing the kernel
+        #     kernel = kernel/np.linalg.norm(kernel)
+
+        #     #Genrating a mask to image
+        #     mask = 255 * kernel
+        #     output = np.copy(conv_img)
+
+        #     # applying the mask to each channel in the input image
+        #     for i in range(3):
+        #         output[:,:,i] = output[:,:,i] * mask
+        #     st.image(output)
+
+        elif filter=='Pencil Sketch':
+            gray_scale = cv2.cvtColor(conv_img, cv2.COLOR_RGB2GRAY)
+            inv_gray = 255 - gray_scale
+            slider = st.sidebar.slider('Intensity: ', 25, 255, 125, step=2)
+            blur_image = cv2.GaussianBlur(inv_gray, (slider,slider), 0, 0)
+            sketch = cv2.divide(gray_scale, 255 - blur_image, scale=256)
+            st.image(sketch, width=300) 
+        
+        elif filter=='Sharpen':
+            i = st.sidebar.selectbox(label='Type: ', options=['Normal','Subtle','Excessive'])
+            if i == 'Normal':
+                kernel = np.array([[-1.,-1.,-1.],[-1.,9.,-1.],[-1.,-1.,-1.]])
+            elif i == 'Subtle':
+                kernel = np.array([[-1.,-1.,-1.,-1.,-1.],[-1., 2.,2.,2.,-1.],[-1.,2.,8.,2.,-1.],[-1.,2.,2.,2.,-1.],[-1.,-1.,-1.,-1.,-1.]])
+            else:
+                kernel = np.array([[1.,1.,1.],[1.,-7.,1.],[1.,1.,1.]])
+            
+            kernel /= kernel.sum()
+            sharp_img = cv2.filter2D(conv_img, -1, kernel)
+            st.image(sharp_img, channels='RGB', width=300)
+
+
+
+
+
+
 st.markdown("<p style='text-align:center'>You can right-click and save the image if you want to!</p>",unsafe_allow_html=True)
+
+
+footer="""
+<style>
+a:link , a:visited{
+color: white;
+background-color: transparent;
+text-decoration: underline;
+}
+.footer {
+display: flex;
+justify-content: left;
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: #0E1117;
+color: white;
+text-align: left;
+}
+</style>
+<div class="footer">
+<p>Developed by <a style='text-align: left;' href="https://github.com/horizon3902/" target="_blank">Kshitij Agarkar</a></p>
+<a style="margin-left: 70%" href="https://github.com/horizon3902/nyc-collisions-analysis" data-color-scheme="no-preference: dark; light: light; dark: dark;" aria-label="Watch horizon3902/movie-recommender-salsa on GitHub">View on Github</a>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
 
 
